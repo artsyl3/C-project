b01e10da28fb4d98c952f6b7c3517129e595e89a
Hossein Ghanbari <118113234+artsyl3@users.noreply.github.com>
2025-09-09 10:41:26 +0200

    Update Program.cs

--- ChallengeProject/Starter/Program.cs
+++ ChallengeProject/Starter/Program.cs
@@ -13,7 +13,7 @@ This C# console application is designed to:
 
     Student         Grade
 
-    Sophia:         92.2    A-
+    Sophia:         93.2    A-
     Andrew:         89.6    B+
     Emma:           85.6    B
     Logan:          91.2    A-
